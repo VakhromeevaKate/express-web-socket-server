4302880c42566927ab3b650ea6fb01bd41b6e815
Ekaterina A Vakhromeeva <vakhromeevae@gmail.com>
2019-06-18 19:44:33 +0300

    feat (server.ts) added some connection behavior

--- server.ts
+++ server.ts
@@ -10,22 +10,79 @@ const server = http.createServer(app);
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
+interface ExtWebSocket extends WebSocket {
+    isAlive: boolean;
+}
+
+function createMessage(content: string, isBroadcast = false, sender = 'NS'): string {
+    return JSON.stringify(new Message(content, isBroadcast, sender));
+}
+
+export class Message {
+    constructor(
+        public content: string,
+        public isBroadcast = false,
+        public sender: string
+    ) { }
+}
+
 wss.on('connection', (ws: WebSocket) => {
 
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
+    const extWs = ws as ExtWebSocket;
 
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+    extWs.isAlive = true;
+
+    ws.on('pong', () => {
+        extWs.isAlive = true;
+    });
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (msg: string) => {
+
+        const message = JSON.parse(msg) as Message;
+
+        setTimeout(() => {
+            if (message.isBroadcast) {
+
+                //send back the message to the other clients
+                wss.clients
+                    .forEach(client => {
+                        if (client != ws) {
+                            client.send(createMessage(message.content, true, message.sender));
+                        }
+                    });
+
+            }
+
+            ws.send(createMessage(`You sent -> ${message.content}`, message.isBroadcast));
+
+        }, 1000);
+
     });
 
     //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
+    ws.send(createMessage('Hi there, I am a WebSocket server'));
+
+    ws.on('error', (err) => {
+        console.warn(`Client disconnected - reason: ${err}`);
+    })
 });
 
+setInterval(() => {
+    wss.clients.forEach((ws: WebSocket) => {
+
+        const extWs = ws as ExtWebSocket;
+
+        if (!extWs.isAlive) return ws.terminate();
+
+        extWs.isAlive = false;
+        ws.ping(null, undefined);
+    });
+}, 10000);
+
 //start our server
-server.listen(process.env.PORT || 8999, () => {
-    console.log(`Server started on port 8999 :)`);
-    console.log(`Server address : ${JSON.stringify(server.address())}`)
+const port = process.env.PORT || 8999;
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port} :)`);
 });
